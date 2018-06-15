427c8c5455189d493ed308ad102ab66a5e851470
Dennis Joel Mwagiru <dennismwagiru@gmail.com>
2018-06-15 23:25:23 +0300

    Update - added API Request

--- app/src/main/java/joels/com/projectsunshine/MainActivity.java
+++ app/src/main/java/joels/com/projectsunshine/MainActivity.java
@@ -1,14 +1,81 @@
 package joels.com.projectsunshine;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.net.URL;
+
+import joels.com.projectsunshine.data.SunshinePreferences;
+import joels.com.projectsunshine.utilities.NetworkUtils;
+import joels.com.projectsunshine.utilities.OpenWeatherJsonUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mWeatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
+        mWeatherTextView = findViewById(R.id.tv_weather_data);
+
+        loadWeather();
+    }
+
+    private void loadWeather(){
+        String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        new FetchWeatherTask().execute(location);
+
+    }
+
+    // Class that extends AsyncTask to perform network calls
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+
+        // Override doInBackgroud method to perform your network requests
+        @Override
+        protected String[] doInBackground(String... params) {
+
+            // If there's no zip code, there's nothing to look up
+            if (params.length == 0){
+                return null;
+            }
+
+            String location = params[0];
+            URL weatherRequestUrl = NetworkUtils.buildUrl(location);
+
+            try {
+                String jsonWeatherResponse = NetworkUtils.getResponseFromHttpUrl(weatherRequestUrl);
+
+                String[] simpleJsonWeatherData = OpenWeatherJsonUtils.getSimpleWeatherStringsFromJson(MainActivity.this, jsonWeatherResponse);
+
+                return simpleJsonWeatherData;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        // Override the onPostExecute method to display the results of the network request
+
+        @Override
+        protected void onPostExecute(String[] weatherData) {
+            if (weatherData != null) {
+                /*
+                * Iterate through the array and append the String to the TextView. The reason why we
+                * add the "\n\n\n" after the STring is to give visuak separation between each String in the TextView.
+                 */
+                for (String weatherString : weatherData) {
+                    mWeatherTextView.append(weatherString + "\n\n\n");
+                }
+            }
+        }
     }
 }
--- app/src/main/java/joels/com/projectsunshine/utilities/NetworkUtils.java
+++ app/src/main/java/joels/com/projectsunshine/utilities/NetworkUtils.java
@@ -1,8 +1,12 @@
 package joels.com.projectsunshine.utilities;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -50,8 +54,22 @@ public final class NetworkUtils {
      * @return The URL to use to query the weather server.
      */
     public static URL buildUrl(String locationQuery) {
-        /** This will be implemented in a future lesson **/
-        return null;
+        Uri builtUril = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                .appendQueryParameter(QUERY_PARAM, locationQuery)
+                .appendQueryParameter(FORMAT_PARAM, format)
+                .appendQueryParameter(UNITS_PARAM, units)
+                .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                .build();
+        URL url = null;
+        try {
+            url = new URL(builtUril.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Log.v(TAG, "Built URI " + url);
+
+        return url;
     }
 
     /**
